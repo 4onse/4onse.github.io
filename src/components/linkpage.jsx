fae389e1001b1936c6086cb5812fec004b569c5d
danistrigaro <daniele.strigaro@gmail.com>
2017-01-20 17:07:49 +0100

    website updates

--- src/components/linkpage.jsx
+++ src/components/linkpage.jsx
@@ -39,7 +39,7 @@ const LinkPage = React.createClass({
                   <td><strong>Twitter</strong></td>
                   <td>It is used to share news and promote the project activites</td>
                 </tr>
-                <tr style={{cursor: "pointer"}} onClick={this.handleClick.bind(this, 'https://www.linkedin.com/in/snsf-4onse-366a45130')}>
+                <tr style={{cursor: "pointer"}} onClick={this.handleClick.bind(this, 'https://www.linkedin.com/groups/8579328')}>
                   <td><strong>Linkedin</strong></td>
                   <td>It is used to share news and promote the project activites</td>
                 </tr>
