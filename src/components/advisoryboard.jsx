c48e2764c2137077f75cf3b54a72fc16f4f6357d
Daniele Strigaro <daniele.strigaro@gmail.com>
2017-08-08 11:43:58 +0200

    added dialog onClick

--- src/components/advisoryboard.jsx
+++ src/components/advisoryboard.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 // react search filter
 import SearchInput, {createFilter} from 'react-search-input'
 //materia ui
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
 import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
 //bootstrap
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
@@ -31,7 +34,26 @@ const KEYS_TO_FILTERS = ['name', 'partner', 'role']
 
 const People = React.createClass({
   getInitialState () {
-    return { searchTerm: '' }
+    return {
+      searchTerm: '',
+      dialog: {
+        open: false,
+        data: <p></p>
+      }
+    }
+  },
+  handleOpen (contact, el) {
+    this.setState(
+      {
+        dialog: {
+          open: true,
+          data: contact
+        }
+      }
+    );
+  },
+  handleClose () {
+    this.setState({dialog: { open: false, data: '' }});
   },
   searchUpdated (term) {
     this.setState({searchTerm: term})
@@ -63,32 +85,32 @@ const People = React.createClass({
         address: <p>Ufficio dei corsi d'acqua<br/>
                  <em>Via Franco Zorzi 13, 6501 Bellinzona</em></p>,
         avatar: noImage,
-        description: <p>Experienced Environmental Engineer with a demonstrated history of 
-        working in the government administration industry. Strong community and social 
-        services professional skilled in HEC-RAS, Sustainable Development, ArcGIS, 
+        description: <p>Experienced Environmental Engineer with a demonstrated history of
+        working in the government administration industry. Strong community and social
+        services professional skilled in HEC-RAS, Sustainable Development, ArcGIS,
         Environmental Impact Assessment, and Environmental Awareness.</p>
       },
       {
         name: 'Prof. Rohinton Emmanuel',
         role: 'Glasgow Caledonia University (UK)',
-        partner: 'Director of The BEAM Research Centre',
+        partner: 'Head of the Sustainable Urban Environment Research Group (SUE-RG)',
         address: <p></p>,
         avatar: noImage,
         description: <p>Professor Rohinton Emmanuel is Professor in Sustainable Design and
-        Construction in Glasgow Caledonia University and has over 20 years of University teaching 
-        experience in Sri Lanka, Sweden, USA and UK. As an Architect with urban design interests, 
+        Construction in Glasgow Caledonia University and has over 20 years of University teaching
+        experience in Sri Lanka, Sweden, USA and UK. As an Architect with urban design interests,
         Rohinton pioneered the inquiry of urban overheating in tropical cities and has taught and
-        consulted on climate, energy and environment sensitive design, building and urban 
-        sustainability and its assessment, urban heat islands and energy efficiency. 
+        consulted on climate, energy and environment sensitive design, building and urban
+        sustainability and its assessment, urban heat islands and energy efficiency.
         Rohinton leads the Sustainable Urban Environment Research Group (SUE-RG) and is an elected
         member of and Secretary to the Board of the International Association for Urban Climate
-        (IAUC). He is also a member of a World Meteorological Organization Expert Team on Urban 
-        and Building Climatology and the Working Group on ‘Climate Change and the Built 
+        (IAUC). He is also a member of a World Meteorological Organization Expert Team on Urban
+        and Building Climatology and the Working Group on ‘Climate Change and the Built
         Environment’ (W108) of the International Council for Buildings (CIB). He had designed LEED
-        certified buildings and has consulted widely in the area of energy efficiency and 
-        sustainability in the built environment. Current research includes the development of the 
-        next generation of building sustainability assessment systems, natural building materials 
-        and processes for energy efficiency in existing buildings and urban heat island 
+        certified buildings and has consulted widely in the area of energy efficiency and
+        sustainability in the built environment. Current research includes the development of the
+        next generation of building sustainability assessment systems, natural building materials
+        and processes for energy efficiency in existing buildings and urban heat island
         measurement in Glasgow, funded by UK and EU funding bodies.</p>
       },
       {
@@ -97,18 +119,18 @@ const People = React.createClass({
         partner: 'Director of The BEAM Research Centre',
         address: <p></p>,
         avatar: noImage,
-        description: <p>Prem Wattage is an independent consultant dedicated to the intelligent use 
-        of natural resources with over thirty years of experience in environmental economics 
-        research and management. In addition to working with the Centre for the Economics and 
-        Management of Aquatic Resources (CEMARE), University of Portsmouth, he has conducted work 
-        for the EU, FAO, WB, ADB, CBD, UNDP, WORLD FISH and UN HABITAT and has undertaken projects 
-        for the private sector. Working internationally, his knowledge extends across a variety of 
-        natural resources including forests, water, wetlands, coasts, fisheries and tree crops. He 
-        regularly publishes resource valuation-related articles in peer-reviewed journals, books 
-        and conference proceedings. He also possesses a specialised knowledge of data collection, 
-        econometrics and modelling of data, as well as knowledge and experience of several 
-        software packages. His current research / consultancy interests are on payments for 
-        ecosystems services, valuation, conservation and support for well-being of local 
+        description: <p>Prem Wattage is an independent consultant dedicated to the intelligent use
+        of natural resources with over thirty years of experience in environmental economics
+        research and management. In addition to working with the Centre for the Economics and
+        Management of Aquatic Resources (CEMARE), University of Portsmouth, he has conducted work
+        for the EU, FAO, WB, ADB, CBD, UNDP, WORLD FISH and UN HABITAT and has undertaken projects
+        for the private sector. Working internationally, his knowledge extends across a variety of
+        natural resources including forests, water, wetlands, coasts, fisheries and tree crops. He
+        regularly publishes resource valuation-related articles in peer-reviewed journals, books
+        and conference proceedings. He also possesses a specialised knowledge of data collection,
+        econometrics and modelling of data, as well as knowledge and experience of several
+        software packages. His current research / consultancy interests are on payments for
+        ecosystems services, valuation, conservation and support for well-being of local
         communities..</p>
       },
       {
@@ -117,20 +139,47 @@ const People = React.createClass({
         partner: 'Disaster Risk Management Specialist',
         address: <p></p>,
         avatar: noImage,
-        description: <p>Vievien has experience in the geospatial industry as project manager, 
-        consultant, and researcher and a particularly strong experience in GIS, geovisualization, 
-        collaborative and community mapping, databases, software development, web design, 
-        usability, project and client management. Vivien has a broad-based scientific 
-        multidisciplinary curriculum with a specialization in GIS. Excellent verbal and written 
-        communication skills with fluency in French, English and intermediate level in Spanish and 
+        description: <p>Vievien has experience in the geospatial industry as project manager,
+        consultant, and researcher and a particularly strong experience in GIS, geovisualization,
+        collaborative and community mapping, databases, software development, web design,
+        usability, project and client management. Vivien has a broad-based scientific
+        multidisciplinary curriculum with a specialization in GIS. Excellent verbal and written
+        communication skills with fluency in French, English and intermediate level in Spanish and
         Open Source software enthusiast, supporter and contributor</p>
       },
-      
+
 
     ]
     const filteredContacts = contactsData.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+    const actions = [
+      <FlatButton
+        label="Close"
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
     return (
       <div id='page-top'>
+        <Dialog
+          modal={false}
+          open={this.state.dialog.open}
+          onRequestClose={this.handleClose}
+          bodyStyle={{padding: '0px'}}
+        >
+          <Card>
+            <CardHeader
+              avatar={this.state.dialog.data.avatar}
+              title={this.state.dialog.data.name}
+              subtitle={<div>{this.state.dialog.data.role}<br />{this.state.dialog.data.partner}</div>}
+            />
+            <CardText>
+              {this.state.dialog.data.description}
+            </CardText>
+            <CardActions>
+              {actions}
+            </CardActions>
+          </Card>
+        </Dialog>
         {/*<div style={styleCarouselItem}></div>*/}
         <div className='container' style={{paddingBottom: '10px'}}>
           <h2 className='title-page'>Contact</h2>
@@ -139,8 +188,8 @@ const People = React.createClass({
           <div className='team-container' >
             <Row>
               {filteredContacts.filter((contact, index) => index<=3 ).map((contact, index) => (
-                <Col md={3} sm={6} key={'key-people-'+index}>
-                  <div className='team-member' >
+                <Col md={3} sm={6} key={'key-people-'+index} >
+                  <div className='team-member' onClick={this.handleOpen.bind(this, contact)}>
                     <img role='presentation' src={contact.avatar} width='100%' />
                     <figcaption>
                       <p className='member-name' style={{fontSize:'12px'}}>{contact.name}</p>
@@ -154,7 +203,7 @@ const People = React.createClass({
             <Row>
               {filteredContacts.filter((contact, index) => index>3 && index <= 7 ).map((contact, index) => (
                 <Col md={3} sm={6} key={'key-people-'+index}>
-                  <div className='team-member' >
+                  <div className='team-member' onClick={this.handleOpen.bind(this, contact)}>
                     <img role='presentation' src={contact.avatar} width='100%' />
                     <figcaption>
                       <p className='member-name' style={{fontSize:'12px'}}>{contact.name}</p>
@@ -168,7 +217,7 @@ const People = React.createClass({
             <Row>
               {filteredContacts.filter((contact, index) => index>7 && index <= 11 ).map((contact, index) => (
                 <Col md={3} sm={6} key={'key-people-'+index}>
-                  <div className='team-member' >
+                  <div className='team-member' onClick={this.handleOpen.bind(this, contact)}>
                     <img role='presentation' src={contact.avatar} width='100%' />
                     <figcaption>
                       <p className='member-name' style={{fontSize:'12px'}}>{contact.name}</p>
@@ -182,7 +231,7 @@ const People = React.createClass({
             <Row>
               {filteredContacts.filter((contact, index) => index > 11 ).map((contact, index) => (
                 <Col md={3} sm={6} key={'key-people-'+index}>
-                  <div className='team-member' >
+                  <div className='team-member'onClick={this.handleOpen.bind(this, contact)}>
                     <img role='presentation' src={contact.avatar} width='100%' />
                     <figcaption>
                       <p className='member-name' style={{fontSize:'12px'}}>{contact.name}</p>
